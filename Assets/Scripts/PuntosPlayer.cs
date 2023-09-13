d9a0b0b31b8b2e1ecee42ccdaa871bc4571dd452
Taranne9 <jeiaramos95@gmail.com>
2023-09-13 18:47:31 -0500

    Correciones

--- Assets/Scripts/PuntosPlayer.cs
+++ Assets/Scripts/PuntosPlayer.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 
 public class PuntosPlayer : MonoBehaviour
 {
 
     public int puntosPlayerActual;
-   
-    
+    public TMP_Text TextoPuntos;
+    public TMP_Text TextoPuntos1;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,5 +28,7 @@ public class PuntosPlayer : MonoBehaviour
 
     public void puntos(int puntosRecividos ) {
         puntosPlayerActual += puntosRecividos;
+        TextoPuntos.text = puntosPlayerActual.ToString();
+        TextoPuntos1.text = puntosPlayerActual.ToString();
     }
 }
